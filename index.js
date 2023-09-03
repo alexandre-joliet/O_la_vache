0b0079e85d9fdf91b3e181c5b921e43ca647e375
alexandre-joliet <alexandre.joliet@gmail.com>
2023-09-03 22:27:54 +0200

    V1 done

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-playAudio = () => {
+const button = document.querySelector('#button');
+
+button.addEventListener('click', playAudio);
+
+function playAudio() {
     new Audio('./sound/olavache.mp3').play();
+}
+
+const theme_button = document.querySelector('#dark_theme');
+
+theme_button.addEventListener('click', themeSwitch)
+
+function themeSwitch () {
+    const body = document.querySelector('body');
+    body.classList.toggle('body__dark');
+    const icon = document.querySelector('#theme_icon')
+    icon.classList.toggle('dark_picture')
 }
